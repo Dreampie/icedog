38dc28836aaeb8015a4ff055c6c904ffffc84dfb
dreampie <wangrenhui1990@gmail.com>
2014-09-26 13:34:50 +0800

    captcha alpha

--- src/main/java/org/icedog/common/web/controller/Controller.java
+++ src/main/java/org/icedog/common/web/controller/Controller.java
@@ -72,20 +72,24 @@ public class Controller extends com.jfinal.core.Controller {
     }
     if (fontsize > 0)
       captcha.setFontSize(fontsize, fontsize);
-    //干扰线数量
-    captcha.setLineNum(4);
-    //噪点数量
-    captcha.setArtifactNum(100);
+    //干扰线数量 默认0
+    captcha.setLineNum(2);
+    //噪点数量 默认50
+    captcha.setArtifactNum(30);
     //使用字符  去掉0和o  避免难以确认
 //    captcha.setCode("ABCDEFGHIJKLMNPQRSTUVWXYZ123456789");
     //验证码在session里的名字 默认 captcha,创建时间为：名字_time
 //    captcha.setCaptchaName("captcha");
-    //验证码颜色
+    //验证码颜色 默认黑色
 //    captcha.setDrawColor(new Color(255,0,0));
-    //背景干扰物颜色
+    //背景干扰物颜色  默认灰
 //    captcha.setDrawBgColor(new Color(0,0,0));
-    //背景色 透明度 前三位数字是rgb色，第四个数字是透明度
+    //背景色+透明度 前三位数字是rgb色，第四个数字是透明度  默认透明
 //    captcha.setBgColor(new Color(225, 225, 0, 100));
+    //滤镜特效 默认随机特效 //曲面Curves //大理石纹Marble //弯折Double //颤动Wobble //扩散Diffuse
+//    captcha.setFilter(CaptchaRender.FilterFactory.Curves);
+    //随机色  默认黑验证码 灰背景元素
+    captcha.setRandomColor(true);
     render(captcha);
   }
 
